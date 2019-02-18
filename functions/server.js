c517790c58a474ca337737100d5ffa9b68af5c3e
Huy Giang <gnhuy91@gmail.com>
2019-02-18 15:11:22 +0700

    update

--- functions/server.js
+++ functions/server.js
@@ -2,10 +2,9 @@ const awsServerlessExpress = require('aws-serverless-express')
 const page = require('../.next/serverless/pages/index')
 const server = awsServerlessExpress.createServer((req, res) => page.render(req, res))
 
-exports.handler = async (event, context, callback) => {
+exports.handler = (event, context, callback) => {
   console.dir(event)
-
   console.log(page)
 
-  awsServerlessExpress.proxy(server, event,context, 'CALLBACK',callback)
+  awsServerlessExpress.proxy(server, event, context, 'CALLBACK', callback)
 }
