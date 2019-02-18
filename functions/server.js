fc07c97fc1070fcc9d4bfcd568cc5000257abad2
Huy Giang <gnhuy91@gmail.com>
2019-02-18 13:46:31 +0700

    update

--- functions/server.js
+++ functions/server.js
@@ -1,5 +1,5 @@
 const awsServerlessExpress = require('aws-serverless-express')
-const page = require('./.next/serverless/index')
+const page = require('../.next/serverless/index')
 const server = awsServerlessExpress.createServer((req, res) => page.render(req, res))
 
 exports.handler = (event, context) => {
