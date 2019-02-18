e758755c1d2c26017be4a1f004659a3f939b0d61
Huy Giang <gnhuy91@gmail.com>
2019-02-18 17:39:04 +0700

    update

--- functions/server.js
+++ functions/server.js
@@ -1,4 +1,5 @@
 const url = require('url')
+const q = require('query-string')
 const awsServerlessExpress = require('aws-serverless-express')
 const page = require('./.next/serverless/pages/index')
 const server = awsServerlessExpress.createServer((req, res) => page.render(req, res))
@@ -8,7 +9,7 @@ exports.handler = (event, context, callback) => {
   console.log(event.headers.referer)
 
   const reqUrl = url.parse(event.headers.referer)
-  console.log(reqUrl)
+  console.log(reqUrl.pathname, q.parse(reqUrl.search))
 
   // awsServerlessExpress.proxy(server, event, context, 'CALLBACK', callback)
   awsServerlessExpress.proxy(server, event, context)
