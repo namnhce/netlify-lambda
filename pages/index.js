2b3123960e089024d93b22a6008624222fa9b933
Huy Giang <gnhuy91@gmail.com>
2019-02-18 18:12:34 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Head from 'next/head'
 
 function Home() {
-  return <div>Welcome to next.js!</div>
+  return <div>
+           <Head>
+             <title>My page title</title>
+             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+           </Head>
+           Welcome to next.js!</div>
 }
 
 export default Home
